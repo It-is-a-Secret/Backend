d403329ca2ef1ddaee121435536d03312eb1333f
zldzldzz <zldzldzz@gs.anyang.ac.kr>
2026-05-12 14:48:30 +0900

    리팩토링: DataResponse 팩토리 메서드 수정
    - 상황에 따라 상태 코드를 사용하도록 수정

--- src/main/java/com/blursome/blursome/global/response/DataResponse.java
+++ src/main/java/com/blursome/blursome/global/response/DataResponse.java
@@ -10,15 +10,25 @@ public class DataResponse<T> extends BaseResponse {
 
   private final T data;
 
-  public DataResponse(HttpStatus status, T data) {
+  private DataResponse(HttpStatus status, T data) {
     super(status);
     this.data = data;
   }
 
-  public static <T> DataResponse<T> from(T data) {
+  // 가장 흔한 케이스 (200 OK)
+  public static <T> DataResponse<T> ok(T data) {
     return new DataResponse<>(HttpStatus.OK, data);
   }
 
+  // 생성이 성공한 케이스 (201 Created)
+  public static <T> DataResponse<T> created(T data) {
+    return new DataResponse<>(HttpStatus.CREATED, data);
+  }
+
+  // 만약 유동적인 상태값이 필요하다면 (최소한의 노출)
+  public static <T> DataResponse<T> of(T data, HttpStatus status) {
+    return new DataResponse<>(status, data);
+  }
   /**
    * data가 null인 경우 아래 같이 사용합니다
    * ResponseEntity.noContent().build()
